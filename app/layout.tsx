1b9ada579985b68c7c238313ff9c0ca2b586b4d6
Jason Weng <jasonweng95@gmail.com>
2023-08-16 00:44:14 +1000

    Auth UI; MongoDB and Prisma setup (Register)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito} from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
-const font = Nunito({ subsets: ['latin'] })
+
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
+
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
