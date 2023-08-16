1b12208c943dfdb85d3659f5a4243420956f6355
Jason Weng <jasonweng95@gmail.com>
2023-08-16 13:53:37 +1000

    Login and Signout function

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from "./components/navbar/Navbar";
 import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 
 const font = Nunito({ subsets: ["latin"] });
@@ -14,18 +16,20 @@ export const metadata: Metadata = {
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+  
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
-        <Navbar />
-
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
