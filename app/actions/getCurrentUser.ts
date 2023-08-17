d050d6fe8a80e54a75c6a2796cd69410107796c1
Jason Weng <jasonweng95@gmail.com>
2023-08-17 23:55:39 +1000

    Enable google and github login

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -26,6 +26,10 @@ export default async function getCurrentUser() {
       return null
     }
 
-    return currentUser;
-  } catch(error: any) {return null};
+    return {...currentUser, 
+    createdAt: currentUser.createdAt.toISOString(), 
+    updatedAt: currentUser.updatedAt.toISOString(),
+    emailVerified: currentUser.emailVerified?.toISOString() || null};
+  } 
+  catch(error: any) {return null};
 }
